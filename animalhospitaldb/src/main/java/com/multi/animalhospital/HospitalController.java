e8cff171c29626f953e4c5726e60725a4ab43475
student <student@M130421>
2020-08-24 14:24:27 +0900

    commit

--- animalhospitaldb/src/main/java/com/multi/animalhospital/HospitalController.java
+++ animalhospitaldb/src/main/java/com/multi/animalhospital/HospitalController.java
@@ -1,10 +1,15 @@
 package com.multi.animalhospital;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.multi.animalhospitalDAO.HospitalDAO;
 
 
 
 @Controller
 public class HospitalController {
+	@Autowired
+	HospitalDAO dao;
 	
 }
