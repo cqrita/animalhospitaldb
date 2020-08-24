63923a2676879b7f2c85ea306230057616763058
student <student@M130421>
2020-08-24 14:26:25 +0900

    commit

---
+++ animalhospitaldb/src/main/java/com/multi/animalhospitalDAO/HospitalDAO.java
@@ -0,0 +1,48 @@
+package com.multi.animalhospitalDAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Component;
+
+import com.multi.animalhospitalVO.HospitalVO;
+import com.multi.animalhospitalVO.Medical;
+
+@Component("hospital")
+public class HospitalDAO implements MedicalDAO {
+
+	@Override
+	public ArrayList<Medical> searchAddress(String a1, String a2) {
+		ArrayList<Medical> list = new ArrayList<Medical>(); 		
+		try {
+			String sql = "select * from hospital where seq=?";
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
+			PreparedStatement pt = con.prepareStatement(sql);
+			pt.setString(1, a1);
+			pt.setString(2, a2);
+			ResultSet rs = pt.executeQuery();
+			while(rs.next()) {
+				Medical vo = new HospitalVO();
+				vo.setName(rs.getString("name"));
+				vo.setNameAddress(rs.getString("nameAddress"));
+				vo.setSeg(rs.getString("seg"));
+				vo.setStatusCode(rs.getString("statusCode"));
+				vo.setStatusName(rs.getString("statusName"));
+				vo.setTel(rs.getString("tel"));
+				vo.setX(rs.getString("X"));
+				vo.setY(rs.getString("Y"));
+				list.add(vo);
+			}
+			pt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+}
---
+++ animalhospitaldb/src/main/java/com/multi/animalhospitalDAO/MedicalDAO.java
@@ -0,0 +1,9 @@
+package com.multi.animalhospitalDAO;
+
+import java.util.ArrayList;
+
+import com.multi.animalhospitalVO.Medical;
+
+public interface MedicalDAO {
+	public ArrayList<Medical> searchAddress(String a1, String a2);
+}
